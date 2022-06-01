c810b46b1abf24b8de51bec976406f1a2db56bf4
Foremancolin <colinf2399@gmail.com>
2022-05-31 21:51:52 -0400

    2nd commit

--- src/gitReview/gitReview01.java
+++ src/gitReview/gitReview01.java
@@ -5,6 +5,8 @@ public class gitReview01 {
 	public static void main(String[] args) {
 		System.out.print("today we learn github");
 		System.out.print("today is a very good day");
+		System.out.print("today is a very good day");
+		
 		}
 	}
 	
