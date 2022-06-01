4112fb5defdca049e794fa41dabb221f805876cf
Foremancolin <colinf2399@gmail.com>
2022-05-31 21:36:05 -0400

    1st commit

---
+++ src/gitReview/gitReview01.java
@@ -0,0 +1,10 @@
+package gitReview;
+
+public class gitReview01 {
+
+	public static void main(String[] args) {
+		System.out.print("today we learn github");
+		System.out.print("today is a very good day");
+		}
+	}
+	
